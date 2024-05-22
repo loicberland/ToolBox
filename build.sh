96bba09b4a4c8596bc761847bea0c464f33ea4c1
lb <loic.berland@lmba.fr>
2024-05-22 15:24:25 +0200

    first commit

---
+++ build.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Fonction pour afficher l'aide
+function show_help() {
+    echo "Usage: ./build.sh [option]"
+    echo "Options:"
+    echo "  all       Build all"
+    echo "  lmba      Build only lmba"
+    echo "  perso      Build only perso"
+    echo "  help      Display this help message"
+}
+
+# Si aucun argument n'est fourni, afficher l'aide
+if [ $# -eq 0 ]; then
+    show_help
+    exit 1
+fi
+
+# Lire les options
+case $1 in
+    all)
+        # Compiler les deux répertoires
+        echo "Building lmba-toolbox.exe..."
+        go build -o ./_build/lmba-toolbox.exe ./lmba
+        echo "lmba-toolbox built successfully."
+
+        echo "Building perso-toolbox.exe..."
+        go build -o ./_build/perso-toolbox.exe ./perso
+        echo "perso-toolbox.exe built successfully."
+        ;;
+    lmba)
+        # Compiler uniquement lmba-toolbox
+        echo "Building lmba-toolbox.exe..."
+        go build -o ./_build/lmba-toolbox.exe ./lmba
+        echo "lmba-toolbox built successfully."
+        ;;
+    perso)
+        # Compiler uniquement perso
+        echo "Building perso-toolbox.exe..."
+        go build -o ./_build/perso-toolbox.exe ./perso
+        echo "perso-toolbox.exe built successfully."
+        ;;
+    help)
+        # Afficher l'aide
+        show_help
+        ;;
+    *)
+        # Option non reconnue, afficher l'aide
+        echo "Invalid option: $1"
+        show_help
+        exit 1
+        ;;
+esac
---
+++ lmba/cmd/root.go
@@ -0,0 +1,42 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var argNoWait bool
+var version = "0.0.1"
+
+var rootCmd = &cobra.Command{
+	Use:     "lmba-toolBox",
+	Short:   "A toolbox for lmba that combines several useful everyday features.",
+	Long:    ``,
+	Version: version,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("This is the first cobra example")
+	},
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	//disable help command
+	rootCmd.SetHelpCommand(&cobra.Command{
+		Use:    "no-help",
+		Hidden: true,
+	})
+	//disable completion command
+	rootCmd.CompletionOptions.DisableDefaultCmd = true
+	rootCmd.PersistentFlags().BoolVar(&argNoWait, "t", false, "Help message for toggle")
+}
