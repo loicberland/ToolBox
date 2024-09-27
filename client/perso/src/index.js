b4bb2285a7c9df6543575f0cf20c1a3e2df4ca79
lb <loic.berland@lmba.fr>
2024-09-27 16:38:30 +0200

    create first client

--- client/perso/src/index.js
+++ client/perso/src/index.js
@@ -1,25 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App/App'
 
-function App() {
-  const [message, setMessage] = useState('');
+const rootElement = document.getElementById('root'); // Trouvez l'élément 'root'
+const root = ReactDOM.createRoot(rootElement); // Créez un nouveau root
 
-  useEffect(() => {
-    // Effectuer une requête GET au serveur Go
-    axios.get('http://localhost:8080/')
-      .then(response => {
-        setMessage(response.data.message);
-      })
-      .catch(error => {
-        console.error('There was an error fetching the data!', error);
-      });
-  }, []);
-
-  return (
-    <div>
-      <h1>{message}</h1>
-    </div>
-  );
-}
-
-export default App;
+root.render(
+  <App />
+); // Rendre l'application avec le nouveau root
--- client/perso/webpack.config.js
+++ client/perso/webpack.config.js
@@ -1,28 +1,36 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react'],
-                    },
-                },
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.js', '.jsx'],
-    },
-    devtool: 'source-map', // Pour le débogage
-    mode: 'development', // Change à 'production' pour une version finale
+  entry: './src/index.js', // Point d'entrée de l'application
+  output: {
+    path: path.resolve(__dirname, 'dist'), // Dossier de sortie
+    filename: 'bundle.js',  // Nom du fichier bundle
+    clean: true, // Nettoie le dossier de sortie avant chaque build
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/, // Pour tous les fichiers .js et .jsx
+        exclude: /node_modules/, // Exclure le dossier node_modules
+        use: {
+          loader: 'babel-loader' // Utiliser babel-loader
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'], // Extensions à résoudre
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // Modèle HTML pour générer votre page
+    }),
+  ],
+  devtool: 'source-map', // Facilite le débogage du code
+  mode: 'development', // Mode de développement
+  devServer: {
+    port: 4000, // Changer le port ici
+    open: true, // Ouvrir le navigateur automatiquement
+  },
 };
